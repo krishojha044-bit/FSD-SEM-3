469752b9649c77488a14761908749c636601e5ff
krishojha044-bit <krishojha044@gmail.com>
2026-09-24 11:08:40 +0530

    Delete exp2.js

--- exp2.js
+++
@@ -1,9 +0,0 @@
-const EventEmitter=require('events');
-class Button extends EventEmitter {};
-const button = newButton();
-button.on('click ,{} => { 
-    console.log('Button Clicked');
-});
-button.on('')
-
-
